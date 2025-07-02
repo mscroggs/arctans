63741d2abaaf70644d7d552a00cb6cd31694f7d2
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2025-07-02 11:05:01 +0100

    handle infinity

--- arctans/arctans.py
+++ arctans/arctans.py
@@ -1,26 +1,46 @@
+"""Arctans."""
+
 from typing import Any
 import math
 import sympy
+from sympy.core.expr import Expr
 
 
 class ArctanSum:
     """The sum of some arctans."""
 
     def __init__(self, *terms: tuple[Any, Any]):
-        self._terms = [(sympy.S(i), sympy.S(j)) for i, j in terms]
-        self._terms.sort(key=lambda i: i[1])
+        """Initialise."""
+        terms_dict = {}
+        for c, a in terms:
+            c = sympy.S(c)
+            a = sympy.S(a)
+            if a not in terms_dict:
+                terms_dict[a] = sympy.Integer(0)
+            terms_dict[a] += c
+        self._terms = [(j, i) for i, j in terms_dict.items()]
+        maxa = max(j for i, j in self._terms if not j.is_infinite)
+        self._terms.sort(key=lambda i: 2 * maxa if i[1].is_infinite else i[1])
         assert len(set([i[1] for i in self._terms])) == len([i[1] for i in self._terms])
 
-    def __str__(self):
+    def __repr__(self) -> str:
+        """Representation."""
+        return self.__str__()
+
+    def __str__(self) -> str:
+        """Convert to a string."""
         return "ArctanSum(" + " + ".join(f"{i}[{j}]" for i, j in self._terms) + ")"
 
-    def __float__(self):
+    def __float__(self) -> float:
+        """Convert to a float."""
         return sum(float(i) * math.atan(float(j)) for i, j in self._terms)
 
     @property
-    def nterms(self):
+    def nterms(self) -> int:
+        """Number of terms."""
         return len(self._terms)
 
     @property
-    def terms(self):
+    def terms(self) -> list[tuple[Expr, Expr]]:
+        """Terms."""
         return self._terms
